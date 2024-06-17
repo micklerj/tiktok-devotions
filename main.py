805c2eb296e48e09eab7e51bee9be78c56ee5855
micklerj <micklerj@ufl.edu>
2024-06-17 12:39:54 -0400

    first fully working prototype

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
 from moviepy.editor import VideoFileClip, AudioFileClip, TextClip, CompositeVideoClip, CompositeAudioClip, vfx
 import random
-import math
 from elevenlabs.client import ElevenLabs
 from elevenlabs import save
 import moviepy.config as moviepy_config
+import re
+import requests
+import json
+import base64
 
 # Set the path to the ImageMagick executable
 moviepy_config.change_settings({"IMAGEMAGICK_BINARY": "C:/Program Files/ImageMagick-7.1.1-Q16-HDRI/magick.exe"})
@@ -16,16 +19,16 @@ prompt = """
   please generate a random devotion base on a random Bible passage.   
 
   guidelines to formatting:  
-  - choose a passage of 1 or 2 Bible verses.
+  - choose a passage of 1 or 2 Bible verses, and NO more.
   - output the name of the book of the Bible, followed by the chapter number,
-  followed by the verse number(s) that you choose. If multiple verses are
+  followed by the verse number(s) that you choose, followed by a period. If multiple verses are
   chosen, output the word "through" between the 1st and 2nd selected verse
-  numbers (example: Mark 4 1 through 2).
+  numbers (example: Mark 4 1 through 2.).
   - output the verse(s).
   - output an encouraging devotion consisting of 4 sentences.
 
   example output: 
-  John 3 16 through 17  
+  John 3 16 through 17.  
 
   For God so loved the world that he gave his one and only Son, that whoever
   believes in him shall not perish but have eternal life. For God did not send
@@ -45,6 +48,11 @@ eleven_client = ElevenLabs(
   api_key=os.getenv("XI_API_KEY"), 
 )
 
+
+# timestamps for subtitles
+sub_start_times = []
+sub_end_times   = []
+
 # generate the devotion with chatGPT
 def get_devotion():
   response = client.chat.completions.create(
@@ -62,30 +70,78 @@ def get_devotion():
 
   return devotion
 
-# generate the voice over
+# generate the voice over    and    save timestamps
 def get_voice_over(input_text):
 
-  audio = eleven_client.generate(
-    text = input_text,
-    
-    voice = "Daniel"     # Daniel: onwK4e9ZLuTAKqWW03F9,    Josh: TxGEqnHWrfWFTfGW9XjX
+  # Daniel: onwK4e9ZLuTAKqWW03F9,   Josh: TxGEqnHWrfWFTfGW9XjX,   James: ZQe5CZNOzWyzPSCn5a3c,   Michael: flq6f7yk4E4fJM5XTYuZ
+  # TODO: automatically choose random voice, background video, and music
+  VOICE_ID = "flq6f7yk4E4fJM5XTYuZ"  
+  XI_API_KEY = os.getenv("XI_API_KEY")
+
+  url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}/with-timestamps"
+
+  headers = {
+    "Content-Type": "application/json",
+    "xi-api-key": XI_API_KEY
+  }
+
+  data = {
+    "text": ( input_text ),
+    "model_id": "eleven_multilingual_v2",
+    "voice_settings": {
+      "stability": 0.5,
+      "similarity_boost": 0.75
+    }
+  }
+
+  response = requests.post(
+    url,
+    json=data,
+    headers=headers,
   )
 
-  save(audio, "mp/voice_over.mp3")
+  if response.status_code != 200:
+    print(f"Error encountered, status: {response.status_code}, "
+      f"content: {response.text}")
+    quit()
 
-  # slow down the audio
-  audio_clip = AudioFileClip("mp/voice_over.mp3")
-  slowed_clip = audio_clip.fx(vfx.speedx, .9)
-  slowed_clip.write_audiofile("mp/slow_voice_over.mp3")
+  # convert the response which contains bytes into a JSON string from utf-8 encoding
+  json_string = response.content.decode("utf-8")
 
-  # TODO: increase pitch of voice
+  # parse the JSON string and load the data as a dictionary
+  response_dict = json.loads(json_string)
 
-  audio_clip.close()
-  slowed_clip.close()
+  # the "audio_base64" entry in the dictionary contains the audio as a base64 encoded string, 
+  # we need to decode it into bytes in order to save the audio as a file
+  audio_bytes = base64.b64decode(response_dict["audio_base64"])
+
+  # save the voice over mp3 file
+  with open('mp/voice_over.mp3', 'wb') as f:
+    f.write(audio_bytes)
+
+  # the 'alignment' entry contains the mapping between input characters and their timestamps
+  characters = response_dict['alignment']['characters']
+  start_times = response_dict['alignment']['character_start_times_seconds']
+  end_times = response_dict['alignment']['character_end_times_seconds']
+
+  
+  start_of_word_reached = False
+  for char, start, end in zip(characters, start_times, end_times):
+    if (char.isspace() or char == '-') and start_of_word_reached:
+      sub_end_times.append(end)
+      start_of_word_reached = False
+
+    elif not char.isspace() and not start_of_word_reached:
+      sub_start_times.append(start)
+      start_of_word_reached = True
+
+    # print(f"[{char}, {start}, {end}] ", end='')
+
+  sub_end_times.append(end_times[-1])  # end time of last word
 
 
-# create subclip for background with pre-made voice over
-def create_video(video_path, audio_path, music_path, output_path):
+# create the whole video
+def create_video(devo, video_path, audio_path, music_path, output_path):
   audio_clip = AudioFileClip(audio_path)
   audio_length = int(audio_clip.duration) + 1
 
@@ -116,34 +172,13 @@ def create_video(video_path, audio_path, music_path, output_path):
   background_video = background_video.crop(width=vid_w, height=vid_h, x_center=background_video.w/2, y_center=background_video.h/2)
 
   # add subtitles
-  audio_file = open(audio_path, "rb")
-  transcript = client.audio.transcriptions.create(
-    file=audio_file,
-    model="whisper-1",
-    response_format="verbose_json",
-    timestamp_granularities=["word"]
-  )
+  # TODO: maybe have multiple words displayed at a time
   subs = []
-  # print(transcript)
-  prev_end = 0
-  next_start_increase = 0.1
-  for obj in transcript.words:
-    text = obj['word']
-    start = obj['start']
-    end = obj['end']    
-
-    # adjust the start n end times for when the timestamps are wrong
-    start += next_start_increase
-    next_start_increase = 0.0
-    if start != prev_end:
-      start = prev_end
-    if start >= end:
-      end += 0.1
-      next_start_increase = 0.1
-    prev_end = end
+  devo_words = re.findall(r"\b[\w']+\b", devo)
+  for word, start, end in zip(devo_words, sub_start_times, sub_end_times):
 
     txt_clip = TextClip(
-      text,
+      word,
       fontsize=45,
       font="Arial-Bold",
       color='white',
@@ -152,8 +187,6 @@ def create_video(video_path, audio_path, music_path, output_path):
     )
     txt_clip = txt_clip.set_start(start).set_end(end).set_position(('center', 'center'))
     subs.append(txt_clip)
-    # print(f"{text} ", end='')
-  print("\n")
 
   background_video = CompositeVideoClip([background_video] + subs)
 
@@ -168,19 +201,29 @@ def create_video(video_path, audio_path, music_path, output_path):
 
 
 if __name__ == "__main__":
-  video_path      = "mp/minecraft.mp4"           # original source for background video
-  music_path      = "mp/music1.mp3"              # rource for background music
-  voice_over_path = "mp/slow_voice_over.mp3"
-  output_path     = "mp/output3.mp4"
+  video_path      = "mp/background1.mp4"           # original source for background video
+  music_path      = "mp/music2.mp3"                # rource for background music
+  voice_over_path = "mp/voice_over.mp3"
+  output_path     = "mp/output5.mp4"
 
   devo = get_devotion()
   print(devo)
-  get_voice_over(devo)                                                    # creates "mp/voice_over.mp3" and "mp/slow_voice_over.mp3"
-  create_video(video_path, voice_over_path, music_path, output_path)      # creates "mp/output.mp4"
+  get_voice_over(devo)                                                          # creates "mp/voice_over.mp3"
+  create_video(devo, video_path, voice_over_path, music_path, output_path)      # creates "mp/output.mp4"
 
 
 
 # ------------------------------------------------- TESTING -------------------------------------------------
+
+  # devo = """
+  #   Philippians 4 13.
+
+  #   I can do all this through him who gives me strength.
+
+  #   In this powerful verse, we are reminded that through Christ, we have the strength to overcome any challenge or obstacle. 
+  #   Our faith in Him empowers us to face each day with confidence and courage. Let us lean on His promises and trust in His 
+  #   provision, knowing that we are never alone in our struggles.
+  # """
 
   # audio_file = open("mp/slowed_voice_.mp3", "rb")
   # transcript = client.audio.transcriptions.create(
--- tts.py
+++ tts.py
@@ -1,18 +1,106 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-from elevenlabs import save
-from elevenlabs.client import ElevenLabs
+import requests
+import json
+import base64
 
 load_dotenv(find_dotenv())
 
-client = ElevenLabs(
-  api_key=os.getenv("XI_API_KEY"), 
+devo = """Philippians 4 13.
+
+  I can do all this through him who gives me strength.
+
+  In this powerful verse, we are reminded that through Christ, we have the strength to overcome any challenge or obstacle.
+  Our faith in Him empowers us to face each day with confidence and courage. Let us lean on His promises and trust in His
+  provision."""
+
+VOICE_ID = "onwK4e9ZLuTAKqWW03F9"  # onwK4e9ZLuTAKqWW03F9 TxGEqnHWrfWFTfGW9XjX
+XI_API_KEY = os.getenv("XI_API_KEY")
+
+url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}/with-timestamps"
+
+headers = {
+  "Content-Type": "application/json",
+  "xi-api-key": XI_API_KEY
+}
+
+data = {
+  "text": ( devo ),
+  "model_id": "eleven_multilingual_v2",
+  "voice_settings": {
+    "stability": 0.5,
+    "similarity_boost": 0.75
+  }
+}
+
+response = requests.post(
+  url,
+  json=data,
+  headers=headers,
 )
 
-audio = client.generate(
-  text="1, 2, 3.",
-  voice="Daniel",
-)
+if response.status_code != 200:
+  print(f"Error encountered, status: {response.status_code}, "
+    f"content: {response.text}")
+  quit()
 
-save(audio, "test.mp3")
+# convert the response which contains bytes into a JSON string from utf-8 encoding
+json_string = response.content.decode("utf-8")
+
+# parse the JSON string and load the data as a dictionary
+response_dict = json.loads(json_string)
+
+# the "audio_base64" entry in the dictionary contains the audio as a base64 encoded string, 
+# we need to decode it into bytes in order to save the audio as a file
+audio_bytes = base64.b64decode(response_dict["audio_base64"])
+
+with open('mp/test_voice.mp3', 'wb') as f:
+  f.write(audio_bytes)
+
+# the 'alignment' entry contains the mapping between input characters and their timestamps
+# print(response_dict['alignment'])
+
+characters = response_dict['alignment']['characters']
+start_times = response_dict['alignment']['character_start_times_seconds']
+end_times = response_dict['alignment']['character_end_times_seconds']
+
+sub_start_times = []
+sub_end_times = []
+start_of_word_reached = False
+for char, start, end in zip(characters, start_times, end_times):
+  if char.isspace() and start_of_word_reached:
+    sub_end_times.append(end)
+    start_of_word_reached = False
+
+  elif not char.isspace() and not start_of_word_reached:
+    sub_start_times.append(start)
+    start_of_word_reached = True
+
+  print(f"[{char}, {start}, {end}] ", end='')
+
+sub_end_times.append(end_times[-1])  # end time of last word
+
+for i in sub_start_times:
+  print(f'{i}   ', end='')
+for i in sub_end_times:
+  print(f'{i}   ', end='')
+
+
+ # audio = eleven_client.generate(
+  #   text = input_text,
+    
+  #   voice = "Daniel"     # Daniel: onwK4e9ZLuTAKqWW03F9,    Josh: TxGEqnHWrfWFTfGW9XjX,    James: ZQe5CZNOzWyzPSCn5a3c    Michael: flq6f7yk4E4fJM5XTYuZ
+  # )
+
+  # save(audio, "mp/voice_over.mp3")
+
+  # # slow down the audio
+  # audio_clip = AudioFileClip("mp/voice_over.mp3")
+  # slowed_clip = audio_clip.fx(vfx.speedx, .9)
+  # slowed_clip.write_audiofile("mp/slow_voice_over.mp3")
+
+  # # TODO: increase pitch of voice
+
+  # audio_clip.close()
+  # slowed_clip.close()
